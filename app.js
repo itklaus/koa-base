5a26fb71baa843e4e37acdee8b5ec18df14df17f
itklaus <it.thomas@yandex.ru>
2017-10-22 20:49:49 +0300

    add a logger, config and templates

--- app.js
+++ app.js
@@ -6,42 +6,67 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
+var config = require('./config');
+var log = require('./libs/log')(module);
 
 var app = express();
-app.set('port', 3000);
+app.set('views', path.join(__dirname, 'templates'));
+app.set('view engine', 'ejs');
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+app.use(express.favicon());
+if (app.get('env') == 'development') {
+  app.use(express.logger('dev'));
+} else {
+  app.use(express.logger('default'));
+}
+
+app.use(express.json()); 
+
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.session({ secret: 'your secret here' }));
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res, next) {
+  res.render('index', {
+    title: 'To be Young and in love',
+    body: '<b>Text body</b>'
+  });
 });
 
-// Middleware
-app.use(function(req,res, next) {
-  if (req.url == '/') {
-    res.end("Hello");
-  } else {
-    next();
-  }
+http.createServer(app).listen(config.get('port'), function(){
+  log.info('Express server listening on port ' + config.get('port'));
 });
 
-app.use(function(req,res,next) {
-  if (req.url == '/admin') {
-    res.send(new Error('wops, denied'));
-  } else {
-    next();
-  }
-});
+// // Middleware
+// app.use(function(req,res, next) {
+//   if (req.url == '/') {
+//     res.end("Hello");
+//   } else {
+//     next();
+//   }
+// });
 
-app.use(function(req,res,next) {
-  if (req.url == '/test') {
-    res.end('test');
-  } else {
-    next();
-  }
-});
+// app.use(function(req,res,next) {
+//   if (req.url == '/admin') {
+//     res.send(new Error('wops, denied'));
+//   } else {
+//     next();
+//   }
+// });
 
-app.use(function(req,res) {
-  res.send(404, 'Page Not Found Sorry');
-})
+// app.use(function(req,res,next) {
+//   if (req.url == '/test') {
+//     res.end('test');
+//   } else {
+//     next();
+//   }
+// });
+
+// app.use(function(req,res) {
+//   res.send(404, 'Page Not Found Sorry');
+// })
 
 app.use(function(err, req, res, next){
   // NODE_ENV = 'production'
